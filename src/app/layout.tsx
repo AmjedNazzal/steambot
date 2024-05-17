1b56b82856d5b4974040f8d09caa78a04a25764a
AmjedNazzal <amjednazzal@outlook.com>
2024-05-17 11:11:13 +0300

    adding SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,6 @@ import Providers from "./components/providers";
 
 const inter = Roboto({ subsets: ["latin"], weight: "400" });
 
-export const metadata: Metadata = {
-  title: "Steambot",
-  description: "Search the Steam store with the help of AI",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,6 +12,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:title" content="Steambot" />
+        <meta
+          property="og:description"
+          content="Search the Steam store with the help of AI"
+        />
+        <meta
+          property="og:url"
+          content="https://steambot-amjed-nazzals-projects.vercel.app/"
+        />
+        <meta property="og:site_name" content="MyNotes" />
+        <meta
+          property="og:image"
+          content="https://steambot-amjed-nazzals-projects.vercel.app/opengraph-image.jpg"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="image/jpg" />
+      </head>
       <Providers>
         <body className={inter.className}>{children}</body>
       </Providers>
